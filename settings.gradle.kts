b9637febe1d4de3c80bc5c126886d5d8d4e39afa
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-05-30 09:17:03 -0700

    port to 1.5

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "cobblemon-spawn-notification"
+
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+        gradlePluginPortal()
+    }
+}
---
+++ src/main/kotlin/us/timinc/mc/cobblemon/spawnnotification/config/SpawnNotificationConfig.kt
@@ -0,0 +1,30 @@
+package us.timinc.mc.cobblemon.spawnnotification.config
+
+class SpawnNotificationConfig {
+    val broadcastShiny = true
+    val broadcastCoords = true
+    val broadcastBiome = false
+    val playShinySound = true
+    val playShinySoundPlayer = false
+    val announceCrossDimensions = false
+    val broadcastDespawns = false
+
+    val broadcastRange: Int = -1
+    val playerLimit: Int = -1
+
+    val labelsForBroadcast: MutableSet<String> = mutableSetOf("legendary");
+
+    @Suppress("KotlinConstantConditions")
+    val broadcastRangeEnabled: Boolean
+        get() = broadcastRange > 0
+
+    @Suppress("KotlinConstantConditions")
+    val playerLimitEnabled: Boolean
+        get() = playerLimit > 0
+
+    val formatting = mutableMapOf<String, String>(
+        "legendary" to "DARK_PURPLE",
+        "legendary.shiny" to "OBFUSCATED",
+        "shiny" to "YELLOW"
+    )
+}
