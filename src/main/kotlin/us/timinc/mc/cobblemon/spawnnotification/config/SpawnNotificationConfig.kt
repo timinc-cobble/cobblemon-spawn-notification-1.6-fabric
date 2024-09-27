b3ef033cfdbb639bfae7cb7f1489f590b1c29c4a
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-09-27 16:21:14 -0700

    added support for broadcasting buckets
    and more

--- src/main/kotlin/us/timinc/mc/cobblemon/spawnnotification/config/SpawnNotificationConfig.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/spawnnotification/config/SpawnNotificationConfig.kt
@@ -1,5 +1,9 @@
 package us.timinc.mc.cobblemon.spawnnotification.config
 
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import us.timinc.mc.cobblemon.spawnnotification.SpawnNotification.MOD_ID
+
 class SpawnNotificationConfig {
     val broadcastShiny = true
     val broadcastCoords = true
@@ -8,11 +12,13 @@ class SpawnNotificationConfig {
     val playShinySoundPlayer = false
     val announceCrossDimensions = false
     val broadcastDespawns = false
+    val broadcastVolatileDespawns = false
 
     val broadcastRange: Int = -1
     val playerLimit: Int = -1
 
-    val labelsForBroadcast: MutableSet<String> = mutableSetOf("legendary");
+    val labelsForBroadcast: MutableSet<String> = mutableSetOf("legendary")
+    val bucketsForBroadcast: MutableSet<String> = mutableSetOf()
 
     @Suppress("KotlinConstantConditions")
     val broadcastRangeEnabled: Boolean
@@ -22,6 +28,21 @@ class SpawnNotificationConfig {
     val playerLimitEnabled: Boolean
         get() = playerLimit > 0
 
-    val formatting = mutableMapOf<String, String>(
+    val formatting = mutableMapOf(
+        "label.legendary" to "LIGHT_PURPLE",
+        "bucket.ultra-rare" to "AQUA",
+        "shiny" to "GOLD"
     )
+
+    fun getComponent(characteristic: String, vararg props: Any): Text {
+        val result = Text.translatable("$MOD_ID.$characteristic", *props)
+        val possibleFormatting = formatting[characteristic]?.let(Formatting::byName)
+        return if (possibleFormatting != null) result.formatted(possibleFormatting) else result
+    }
+
+    fun getRawComponent(characteristic: String, vararg props: Any): Text {
+        val result = Text.translatable(characteristic, *props)
+        val possibleFormatting = formatting[characteristic]?.let(Formatting::byName)
+        return if (possibleFormatting != null) result.formatted(possibleFormatting) else result
+    }
 }
---
+++ src/main/kotlin/us/timinc/mc/cobblemon/spawnnotification/events/BroadcastDespawn.kt
@@ -0,0 +1,97 @@
+package us.timinc.mc.cobblemon.spawnnotification.events
+
+import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent
+import com.cobblemon.mod.common.api.events.pokemon.PokemonFaintedEvent
+import com.cobblemon.mod.common.api.spawning.CobblemonSpawnPools
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.cobblemon.mod.common.pokemon.Pokemon
+import net.minecraft.entity.Entity
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import us.timinc.mc.cobblemon.spawnnotification.SpawnNotification.config
+import us.timinc.mc.cobblemon.spawnnotification.broadcasters.DespawnBroadcaster
+import us.timinc.mc.cobblemon.spawnnotification.util.Broadcast
+import us.timinc.mc.cobblemon.spawnnotification.util.DespawnReason
+import us.timinc.mc.cobblemon.spawnnotification.util.PlayerUtil.getValidPlayers
+
+object BroadcastDespawn {
+    fun handle(evt: PokemonCapturedEvent) {
+        if (!config.broadcastDespawns) return
+
+        val entity = evt.pokeBallEntity
+        val coords = entity.blockPos
+        val level = entity.world
+        if (level !is ServerWorld) return
+
+        broadcast(
+            evt.pokemon,
+            coords,
+            level.getBiome(coords).key.get().value,
+            level.dimensionKey.value,
+            level,
+            DespawnReason.CAPTURED
+        )
+    }
+
+    fun handle(evt: PokemonFaintedEvent) {
+        if (!config.broadcastDespawns) return
+        if (!evt.pokemon.isWild()) return
+
+        val entity = evt.pokemon.entity ?: return
+        val coords = entity.blockPos
+        val level = entity.world
+        if (level !is ServerWorld) return
+
+        broadcast(
+            evt.pokemon,
+            coords,
+            level.getBiome(coords).key.get().value,
+            level.dimensionKey.value,
+            level,
+            DespawnReason.FAINTED
+        )
+    }
+
+    fun handle(entity: Entity, level: ServerWorld) {
+        if (!config.broadcastVolatileDespawns) return
+        if (entity !is PokemonEntity) return
+
+        val coords = entity.blockPos
+
+        broadcast(
+            entity.pokemon,
+            coords,
+            level.getBiome(coords).key.get().value,
+            level.dimensionKey.value,
+            level,
+            DespawnReason.DESPAWNED
+        )
+    }
+
+    private fun broadcast(
+        pokemon: Pokemon,
+        coords: BlockPos,
+        biome: Identifier,
+        dimension: Identifier,
+        level: ServerWorld,
+        reason: DespawnReason
+    ) {
+        DespawnBroadcaster(
+            pokemon,
+            CobblemonSpawnPools.WORLD_SPAWN_POOL,
+            coords,
+            biome,
+            dimension,
+            reason
+        ).getBroadcast()?.let { message ->
+            if (config.announceCrossDimensions) {
+                Broadcast.broadcastMessage(message)
+            } else if (config.broadcastRangeEnabled) {
+                Broadcast.broadcastMessage(getValidPlayers(level.dimensionKey, coords), message)
+            } else {
+                Broadcast.broadcastMessage(level, message)
+            }
+        }
+    }
+}
